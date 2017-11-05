c18e500b57b91554e7be7e82da5f7c5ee525e850
Raehyang Kim <raekim95@gmail.com>
2017-11-05 09:07:00 +0900

    change paragraph content that pops up when the user typed in his/her name in the prompt

--- happy-doraemon/scripts/main.js
+++ happy-doraemon/scripts/main.js
@@ -5,9 +5,12 @@ var myImage = document.querySelector('img');
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
 	if(mySrc === 'images/image-doraemon.png') {
-      myImage.setAttribute ('src','image-doraemon-eating.png');
+     myImage.setAttribute ('src','images/image-doraemon-eating.png');
+     myImage.setAttribute ('alt','Doraemon enjoying his Dorayaki');
+      
 	} else {
 	  myImage.setAttribute ('src','images/image-doraemon.png');
+    myImage.setAttribute ('alt','Doraemon happily carrying a bag of Dorayakis');
 	}
 }
 
@@ -15,18 +18,26 @@ myImage.onclick = function() {
 
 var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
+var myReason = [document.querySelector('#reason1'), document.querySelector('#reason2'), document.querySelector('#reason3')];
 
 function setUserName() {
   var myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.innerHTML = 'Doraemon is happy for ' + myName +'!';
-}
+  showContent();
+};
+
+function showContent(){
+  var storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Doraemon is happy for ' + storedName +'!';
+  myReason[0].innerHTML = storedName + ' has visited this site';
+  myReason[1].innerHTML = 'He has bunch of Dorayakis to share with ' + storedName;
+  myReason[2].innerHTML = storedName + ' is going to click the image of him above so that he can enjoy his share!';
+};
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
-  var storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Doraemon is happy for ' + storedName +'!';
+  showContent();
 }
 
 myButton.onclick = function() {
